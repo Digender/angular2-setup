3037508a7d4437b24bd4631dba242f7279b13dc0
Digender Mahara <Digender Mahara>
2016-10-15 00:31:26 +0530

    added few more examples

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-@Component({
+@Component ({
   selector: 'my-app',
-  template: `<h1>My First Angular App </h1>
+  template:
+  `<h1>My First Angular App </h1>
   <h3>First Name : {{myJson.fname}}</h3>
   <h3>Last Name : {{myJson.lname}}</h3>
   <h3 (click)="popup(myJson.age)">Age : {{myJson.age}}</h3>
+  <table>
+    <tr (click)="popup(a)" *ngFor="let a of myArr"><td>{{a}}</td></tr>
+  </table>
+  Enter a name and press Enter, also check browser console<br>
+  <input #a (keyup.enter)="callMe(a.value)"/>
+  {{a.value}}
+  <hr>
+  Two way binding :
+  <br>
+  <input type="text" class="form-control" id="name" required [(ngModel)]="model.name" name="name">
+  {{model.name}}
   `
 })
 export class AppComponent {
@@ -13,7 +25,16 @@ export class AppComponent {
     "lname" : "Mahara",
     "age":25
     }
-    popup(age){
+    myArr = [];
+    model = {};
+    popup (age) {
       alert("You clicked my age : "+age);
+    }
+    constructor(){
+       this.myArr = [10,20,30,40,50,60,70,80,90,100];
+       this.model={"name":"Hello"};
+    }
+    callMe(msg: string){
+       console.log("Text : "+msg);
     }
 }
